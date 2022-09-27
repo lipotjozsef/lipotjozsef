8dcda0219112defa869b60eb6b1c9f4115427444
lipotjoci <unconfigured@null.spigotmc.org>
2022-09-27 22:55:22 +0200

    Hub alapjai elkészítve.

--- sources/main.js
+++ sources/main.js
@@ -23,12 +23,10 @@ function draw(e){
 
     ctx.lineWidth = 10;
     ctx.lineCap = "round";
-
-    var offset = 85
-    ctx.lineTo(e.clientX - 50, e.clientY - offset);
+    ctx.lineTo(e.clientX - 90, e.clientY -95);
     ctx.stroke();
     ctx.beginPath();
-    ctx.moveTo(e.clientX - 50, e.clientY - offset);
+    ctx.moveTo(e.clientX - 90, e.clientY - 95);
 }
 
 canvas.addEventListener("mousemove", draw);
@@ -38,8 +36,10 @@ canvas.addEventListener("mouseup", StopPainting);
 
 
 function downloadImg() {
+    var link = document.createElement('a');
     var img = canvas.toDataURL("image/png");
-    btn.download = 'canvas.png';
-    btn.href = img
-    btn.click()
+    link.download = 'canvas.png';
+    link.href = img
+    link.click()
+    link.remove()
 }
