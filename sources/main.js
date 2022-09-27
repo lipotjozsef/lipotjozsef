0d257b52e1f97e5727007f2d7a39754395babec1
lipotjoci <unconfigured@null.spigotmc.org>
2022-09-27 22:40:56 +0200

    Rajztábla hozzáadva.

--- sources/main.js
+++ sources/main.js
@@ -1,30 +1,45 @@
 const canvas = document.getElementById("rajz");
+var btn = document.getElementById("down");
 const ctx = canvas.getContext("2d");
-
 var isDrawing = false;
-var x = event.clientX;     // Get the horizontal coordinate
-var y = event.clientY;     // Get the vertical coordinate
-var coor = "X coords: " + x + ", Y coords: " + y;
-ctx.beginPath();
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 200);
-ctx.strokeStyle = "#fff"
-ctx.stroke();
+var animFinished = false;
 
-addEventListener("mouseup",() => {
-    console.log("Up");
-    isDrawing = true;
-});
-addEventListener("mousedown", () => {
-    console.log("Down");
-    isDrawing = true;
-    drawing()
-});
+window.setTimeout(function () {
+   animFinished = true
+   console.log("Hello")
+}, 2000);
 
-function drawing()
-{
-    if(isDrawing)
-    {
-        console.log(coor);
-    }
+function StartPainting(){
+    isDrawing = true;
+}
+
+function StopPainting(){
+    isDrawing = false;
+    ctx.beginPath();
+}
+
+function draw(e){
+    if(!isDrawing || !animFinished) return;
+
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+
+    var offset = 85
+    ctx.lineTo(e.clientX - 50, e.clientY - offset);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX - 50, e.clientY - offset);
+}
+
+canvas.addEventListener("mousemove", draw);
+
+canvas.addEventListener("mousedown", StartPainting);
+canvas.addEventListener("mouseup", StopPainting);
+
+
+function downloadImg() {
+    var img = canvas.toDataURL("image/png");
+    btn.download = 'canvas.png';
+    btn.href = img
+    btn.click()
 }
