507dd3c78e05f19f411a5d9d831fe4216686349b
lipotjozsef <lipot.jozsef.zoltan@students.jedlik.eu>
2026-04-08 13:42:16 +0200

    Dark mode toggle & structural changes in HMTL

--- munka/scripts/irodalmak.js
+++ munka/scripts/irodalmak.js
@@ -1,18 +1,39 @@
 // Helló, ha ezt olvasod :-) - Joci 2026. 04. 07.
 
+const IsDevicePrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const darkModeClassName = "dark-mode";
 const ulElement = document.querySelector("#link-list");
+
 const usedMaterialLinks = [
     ["Wikipédia", "https://hu.wikipedia.org/wiki/Súrlódás"],
     ["Wikipédia", "https://en.wikipedia.org/wiki/Guillaume_Amontons"],
     ["Netfizika", "https://www.netfizika.hu/tudas/node/14221"],
     ["Netfizika", "https://www.netfizika.hu/tudas/node/14225"],
-    ["BME_Robotalkalmazások", "https://mogi.bme.hu/TAMOP/robotalkalmazasok/ch06.html"]
+    ["BME Robotalkalmazások", "https://mogi.bme.hu/TAMOP/robotalkalmazasok/ch06.html"]
 ];
 
-usedMaterialLinks.forEach( ([name, link], index) => {
-    const newItemElement = document.createElement("li");
 
-    newItemElement.innerHTML = `[${index+1}] ${name.replaceAll("_", " ")}\t- <a href="${link}" target="_blank">\t${link}</a>`;
+document.body.onload = main;
 
-    ulElement.appendChild(newItemElement);
-})
+function main() {
+    fillUpLinks();
+    enableDarkMode();
+}
+
+function fillUpLinks() {
+    usedMaterialLinks.forEach(([name, link], index) => {
+        const newItemElement = document.createElement("li");
+
+        let showIndex = index + 1;
+        let linkHTML = `<a href="${link}" target="_blank">\t${link}</a>`;
+        newItemElement.innerHTML = `[${showIndex}] ${name}\t- ${linkHTML}`;
+    
+        ulElement.appendChild(newItemElement);
+    })
+}
+
+function enableDarkMode() {
+    if(IsDevicePrefersDark) {
+        document.body.classList.add(darkModeClassName);
+    }
+}
